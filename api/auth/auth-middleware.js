965d966feca2f542cb95f861f55b7d7edb7e3b5d
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-17 21:48:46 -0500

    build checkPasswordLength

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -13,46 +13,41 @@ function restricted() {}
 async function checkUsernameFree(req, res, next) {
   const username = { username: req.body.username };
 
-  const [user] = await Users.findBy(username);
+  try {
+    const [user] = await Users.findBy(username);
 
-  if (user?.username) {
-    res.status(422).json({ message: "Username taken" });
-  } else {
-    next();
+    if (user?.username) {
+      res.status(422).json({ message: "Username taken" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next({ message: err.message, status: err.status });
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 async function checkUsernameExists(req, res, next) {
   const username = { username: req.body.username };
 
-  const user = await Users.findBy(username).first();
+  try {
+    const user = await Users.findBy(username).first();
 
-  if (!user.username) {
-    res.status(401).json({ message: "Invalid credentials" });
-  } else {
-    next();
+    if (!user.username) {
+      res.status(401).json({ message: "Invalid credentials" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next({ message: err.message, status: err.status });
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
 function checkPasswordLength(req, res, next) {
   const password = req.body.password;
   if (password && password.length > 3) {
+    next();
+  } else {
+    res.status(422).json({ message: "Password must be longer than 3 chars" });
   }
 }
 
